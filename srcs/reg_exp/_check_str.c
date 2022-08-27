1652f98d3b2ebd23ada85107d4b338558110f7af
Liquorice Ignignokt <lignigno@un-s3.kzn.21-school.ru>
2022-08-27 03:25:31 +0300

    repetition setter has been started and check str has been started

--- srcs/reg_exp/_check_str.c
+++ srcs/reg_exp/_check_str.c
@@ -6,49 +6,76 @@
 /*   By: lignigno <lignign@student.21-school.ru>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 00:42:33 by lignigno          #+#    #+#             */
-/*   Updated: 2022/08/27 02:28:35 by lignigno         ###   ########.fr       */
+/*   Updated: 2022/08/27 03:23:17 by lignigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "_check_regexp_encaps.h"
 
-// static void	set_repeat(const regexp_rules_t * rule, repeat_t * repeat)
-// {
-// 	if (check_flag(rule->hflags, FLAG_REPEATER) == true)
-// 	{
-		
-// 	}
-// 	else
-// 		repeat->to = 1;
-// }
+static void	set_repeater(const regexp_rules_t * rule, repeat_t * repeat)
+{
+	if (check_flag(rule->hflags, FLAG_REPEATER) == true)
+	{
+		/* {from,to} */
+		if (check_flag(rule->hflags, FLAG_FROM | FLAG_TO) == true)
+		{}
+		/* {from,} */
+		else if (check_flag(rule->hflags, FLAG_FROM) == true)
+		{}
+		/* {,to} */
+		else if (check_flag(rule->hflags, FLAG_TO) == true)
+		{}
+		/* {from} */
+		else
+		{}
+	}
+	else
+		repeat->to = 1;
+}
 
 regexp_ret_code_t	check_str(const char * str, const regexp_rules_t * pars_rules)
 {
-	(void)str;
-	(void)pars_rules;
-	// size_t		i;
-	// repeat_t	repeat;
+	size_t		i;
+	repeat_t	repeater;
 
-	// i = 0;
-	// while (str[i] != '\0' && pars_rules != NULL)
-	// {
-	// 	if (check_flag(pars_rules->hflags, FLAG_SINGLE_SYMBOL) == true)
-	// 	{
-	// 		i++;
-	// 	}
-	// 	if (check_flag(pars_rules->hflags, FLAG_SIMPLE) == true)
-	// 	{
-	// 		// while (repeat.to > 0)
-	// 		// {
-	// 		// 	/* code */
-	// 		// }
-			
-	// 	}
+	i = 0;
+	while (str[i] != '\0' && pars_rules != NULL)
+	{
+		if (check_flag(pars_rules->hflags, FLAG_SINGLE_SYMBOL) == true)
+		{
+			set_repeater(pars_rules, &repeater);
+			while (repeater.to > 0 && str[i] != '\0')
+			{
+				if (check_single_symbol(str, i) == false)
+				{
+					break ;
+				}
+				repeater.from -= (repeater.from > 0);
+				--repeater.to;
+			}
+			if (repeater.from > 0)
+				return (REGEXP_FAIL);
+		}
+		if (check_flag(pars_rules->hflags, FLAG_SIMPLE) == true)
+		{
+			set_repeater(pars_rules, &repeater);
+			while (repeater.to > 0 && str[i] != '\0')
+			{
+				if (check_simple(str, i) == false)
+				{
+					break ;
+				}
+				repeater.from -= (repeater.from > 0);
+				--repeater.to;
+			}
+			if (repeater.from > 0)
+				return (REGEXP_FAIL);
+		}
 
-	// 	pars_rules = pars_rules->next;
-	// }
+		pars_rules = pars_rules->next;
+	}
 
-	// if (str[i] == '\0')
+	if (str[i] == '\0')
 		return (REGEXP_OK);
-	// return (REGEXP_FAIL);
+	return (REGEXP_FAIL);
 }
